d810b523a069c9a2ad46678018a1aeee79457153
TheCoderRiya <riyasahu7709@gmail.com>
2021-08-31 16:24:03 +0530

    new chnages added

--- src/main/java/com/sahu/DemoSampleProjectSbmsApplication.java
+++ src/main/java/com/sahu/DemoSampleProjectSbmsApplication.java
@@ -2,12 +2,15 @@ package com.sahu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoSampleProjectSbmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSampleProjectSbmsApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(DemoSampleProjectSbmsApplication.class, args);
+		
+		run.close();
 	}
 
 }
